7dffff27ecf46c53c58f4c35ca43e147689f949d
Sinan <me@wunnle.com>
2019-12-24 00:41:32 +0300

    debugging title

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -5,6 +5,7 @@ export default function Template({
   data // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
+  console.log({ data })
   const { frontmatter, html } = markdownRemark
   return (
     <div className="blog-post-container">
