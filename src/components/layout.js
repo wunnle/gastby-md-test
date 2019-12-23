023e9a39e56f97910ab2e796f194dfe852f91b01
Sinan <me@wunnle.com>
2019-12-24 00:22:48 +0300

    removing data

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,15 +7,15 @@ import React from 'react'
 import Header from './header'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
